8c0861c68408ace1f10c12157619a672f781dbf7
ShafShafiq <115353189+ShafShafiq@users.noreply.github.com>
2024-04-01 21:27:26 +0500

    Update Cloudinary configuration to use environment variables

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -9,9 +9,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Configure Cloudinary
 cloudinary.config({ 
-    cloud_name: 'dr6pm7eyl', 
-    api_key: '752458116893527', 
-    api_secret: 'eXCzO7AAFTw-XjOscdWX6Fgxff0' 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET
   });
 
 // Configure multer-storage-cloudinary
